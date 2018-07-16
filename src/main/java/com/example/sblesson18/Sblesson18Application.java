57a4e539cc33558faf0784b93b0fe60fbf4c0b4b
Dave Wolf <dave45678@gmail.com>
2018-07-16 17:51:03 -0400

    Initial commit

---
+++ src/main/java/com/example/sblesson18/Sblesson18Application.java
@@ -0,0 +1,12 @@
+package com.example.sblesson18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sblesson18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sblesson18Application.class, args);
+	}
+}
